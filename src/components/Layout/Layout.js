95dffd688405cd7af6fdb6f9165c140430f82ca0
sherryli1 <33634651+sherryli1@users.noreply.github.com>
2018-12-23 18:04:04 -0800

    Layout done

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,34 @@
 //A wrapper to wrap the core elements in the screen
-import React from 'react';
+import React,{ Component } from 'react';
 import Aux from'../../hoc/Aux';
 import classes from'./Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-    <Toolbar/>
-    <main className = {classes.Content}>
-      {props.children}
-    </main>
-    </Aux>
-);
+class Layout extends Component{
+  state = {
+    showSideDrawer : false
+  }
+  toggleSideDrawerHandler = () => {
+    //correct way to change state based on current state
+    this.setState( (prevState) => {
+      return {showSideDrawer : !prevState.showSideDrawer};
+    });
+  }
+  closeSideDrawerHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
+  render () {
+    return(
+      <Aux>
+      <Toolbar toggleDrawerClicked={this.toggleSideDrawerHandler}/>
+      <SideDrawer open={this.state.showSideDrawer} clicked={this.closeSideDrawerHandler}/>
+      <main className = {classes.Content}>
+        {this.props.children}
+      </main>
+      </Aux>
+    );
+  }
+ }
 
-
-export default layout;
+export default Layout;
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
+
+
+const sideDrawer = (props) => {
+  let styleCob = [classes.SideDrawer, classes.Close];
+  if(props.open){
+    styleCob = [classes.SideDrawer, classes.Open];
+  }
+  return(
+    <Aux>
+      <Backdrop show={props.open} closed={props.clicked}/>
+      <div className={styleCob.join(' ')}>
+        <div className={classes.Logo }>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
+  );
+
+};
+
+export default sideDrawer;
